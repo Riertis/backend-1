5661b7cd53b4706f8b4b4573110f9ca8e357b51f
Dmitriy M Riertis <moseij@yandex.ru>
2025-12-06 18:11:59 +0300

    feat: созданы пробные обработчики

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import express from 'express';
+import logger from './logger/pino.logger';
 
 const server = express();
 
 const port = 2000;
 
+server.get('/task', (req, res) => {
+  res.json({ message: 'Задачки' });
+});
+
+server.post('/user/register', (req, res) => {
+  logger.info(`Регистрация нового пользователя`);
+  res.json({ message: 'Регистрация' });
+});
+
+server.post('/user/login', (req, res) => {
+  logger.info(`Вход пользователя`);
+  res.json({ message: 'Вход' });
+});
 server.listen(port, () => {
-  console.log(`Server is started on port ${port}...`);
+  logger.info(`Server is started on port ${port}...`);
 });
